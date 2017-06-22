f7e76beff3dee328972058bfefe26dbc63f7ce65
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 17:19:47 +0000

    Fixed /portfolios route

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,7 +7,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
   
-    def create
+  def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
