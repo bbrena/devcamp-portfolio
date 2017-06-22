f49d7e31bbe4d047d615199a6baaf199d703023e
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 16:31:21 +0000

    Integrated status_toggle via button push

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
-Rails.application.routes.draw do
+  Rails.application.routes.draw do
+    
+  resources :blogs do
+    member do 
+      get :toggle_status
+  end
+    
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
-  resources :blogs
+end
   
   root to: 'pages#home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621154839) do
+ActiveRecord::Schema.define(version: 20170622161718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,10 @@ ActiveRecord::Schema.define(version: 20170621154839) do
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "slug"
-    t.integer  "status"
+    t.integer  "status",     default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
   end
 
