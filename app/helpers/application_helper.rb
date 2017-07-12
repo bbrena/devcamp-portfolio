6fcb7fcda3ebce22933f4f89eeb836cc473d1106
Brena A <ackids@anzerfamilie.com>
2017-07-12 16:50:36 +0000

    Guide to Implementing Petergate in a Rails 5 Application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper
-    if current_user.is_a?(User)
-      link_to "Logout From Your Account", destroy_user_session_path, method: :delete
-    else
+    if current_user.is_a?(GuestUser)
       (link_to "Register For An Account", new_user_registration_path) +
       "<br>".html_safe +
       (link_to "Login To Your Account", new_user_session_path)
+    else
+      link_to "Logout From Your Account", destroy_user_session_path, method: :delete
     end
   end
   
