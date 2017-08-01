d03018222669f49b0f45b99e880bee395dd4fddd
Brena A <ackids@anzerfamilie.com>
2017-08-01 17:27:29 +0000

    Guide to Integrating Markdown and Syntax Highlighting in a Rails 5 Application

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,24 @@ module BlogsHelper
   def gravatar_helper user
     image_tag "https://en.gravatar.com/userimage/125712839/49e26ea8781d5601750a602ec9bca0a0.jpg", width: 60
   end
+  
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
+  
+  def markdown(text)
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)\
+    
+    options = {
+      fenced_code_blocks: true, 
+      no_intra_emphasis: true,
+      autolink: true, 
+      lax_html_blocks: true
+    }
+    
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
+  end
 end
