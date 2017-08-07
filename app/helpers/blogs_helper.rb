93306fa4dbe0c38b9e85f0440f8827c38e9ba6ec
Brena A <ackids@anzerfamilie.com>
2017-08-07 21:40:47 +0000

    Little changes

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,8 +1,5 @@
 module BlogsHelper
-  def gravatar_helper user
-    image_tag "https://en.gravatar.com/userimage/125712839/49e26ea8781d5601750a602ec9bca0a0.jpg", width: 60
-  end
-  
+
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
       CodeRay.scan(code, language).div
