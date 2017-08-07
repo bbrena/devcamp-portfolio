ff1b06dc61de191b0f421b48ea3245368ce32acf
Brena A <ackids@anzerfamilie.com>
2017-08-07 23:09:38 +0000

    Application JS updated.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require bootstrap-sprockets
+//= require jquery3
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require jquery-ui
 //= require html.sortable
 //= require cocoon
