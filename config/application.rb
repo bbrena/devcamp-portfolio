72ff60962b4c869f84878cc92360fcb14c093cf7
Brena A <ackids@anzerfamilie.com>
2017-08-07 19:53:51 +0000

    Trying to fix stuff

--- config/application.rb
+++ config/application.rb
@@ -16,11 +16,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 module DevcampPortfolio
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
-    config.serve_static_assets = true
   end
 end
