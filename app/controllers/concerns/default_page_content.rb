3da2c21a6108f596089aa8582168358b85af9374
Brena A <ackids@anzerfamilie.com>
2017-08-15 03:53:18 +0000

    Changed what it says on the tab

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
   
     def set_page_defaults 
-      @page_title  = "Beading Brena's Jewelry"
-      @seo_keywords = "Beading Brena's Jewelry"
+      @page_title  = "Cost Engineers Inc. Website"
+      @seo_keywords = "cost-engineers, Cost-Engineers, Cost-engineers, cost-Engineers, Cost Engineers, cost Engineers, Cost engineers, Cost-Engineers Inc., Cost-engineers Inc., Cost-engineers inc., cost-engineers inc., cost-engineers Inc."
   end
 end
