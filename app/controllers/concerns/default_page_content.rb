4fa3db1561a16614dd40dbe520701de3cb76db4b
Brena A <ackids@anzerfamilie.com>
2017-08-15 04:23:43 +0000

    Changed what it says on the tab

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
   
     def set_page_defaults 
-      @page_title  = "Cost Engineers, Inc. Website"
+      @page_title  = "Construction Cost Estimating Consulting Estimate Firm"
       @seo_keywords = "Cost Engineers, Inc."
   end
 end
